1b183f728de3524e5c01374214f6708a01cb2baa
Arnon Kahani <arnon.kahani@gmail.com>
2016-06-15 13:18:14 +0300

    Combined Supplier and Storage Interfaces

---
+++ src/com/Common/ISupplierStorage.java
@@ -0,0 +1,51 @@
+package com.Common;
+
+import com.SupplierStorage.BE.OrderProduct;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+/**
+ * Created by arnon on 11/06/2016.
+ */
+public interface ISupplierStorage {
+
+
+    /**
+     * Presents a supplier menu.
+     *
+     * @param
+     * @param
+     * @return
+     */
+    void showSupplier();
+
+    /**
+     * Presents a supplier menu.
+     *
+     * @param
+     * @param
+     * @return
+     */
+    void showStorage();
+
+
+    /**
+     * Receive a supply to storage.
+     *
+     * @param products
+     * @return
+     */
+    void getSupply(ArrayList<OrderProduct> products);
+
+
+    /**
+     * Presents a supplier menu.
+     *
+     * @param first_time is used to indicate the first time the system is loaded in order to create the tables
+     * @param c is the connection for the DB
+     * @return
+     */
+    void initiateSupplierStorage(boolean first_time,Connection c);
+
+}
